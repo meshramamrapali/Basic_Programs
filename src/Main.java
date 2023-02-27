777781cc28901b7cc4e44f3be4dad824565fc99a
meshramamrapali <amrapalimeshram86@gmail.com>
2023-02-27 13:26:44 +0530

    ADDED : Leap Year

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,44 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
 
         System.out.println("Welcome to Basic Programs");
+        System.out.println("----------------------------------------");
 
+           double head = 0, tail = 0;
+            int numberOfAttempts = 5;
+            for (int i = 1; i <= numberOfAttempts; i++) {
+                double rand = Math.random();
+                String coin = "";
+                if (rand < 0.5)
+                    head++;
+                else
+                    tail++;
+            }
+            System.out.println("Head Results:" + head);
+            System.out.println("Tail Results:" + tail);
 
-        double head = 0, tail = 0;
-        int numberOfAttempts = 5;
-        for (int i = 1; i <= numberOfAttempts; i++) {
-            double rand = Math.random();
-            String coin = "";
-            if (rand < 0.5)
-                head++;
+            System.out.println("Head Probability:" + (head) / numberOfAttempts);
+            System.out.println("Tail Probability:" + (tail) / numberOfAttempts);
+            System.out.println("---------------------------------------------------");
+
+            int year;
+            System.out.println("Enter an Year :: ");
+            Scanner sc = new Scanner(System.in);
+            year = sc.nextInt();
+
+            if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+                System.out.println("Specified year is a leap year");
             else
-                tail++;
+                System.out.println("Specified year is not a leap year");
+                System.out.println("-------------------------------------------------");
+
+
         }
-        System.out.println("Head Results:" + head);
-        System.out.println("Tail Results:" + tail);
-
-        System.out.println("Head Probability:" + (head) / numberOfAttempts);
-        System.out.println("Tail Probability:" + (tail) / numberOfAttempts);
-
     }
 
-}
+
+
+
