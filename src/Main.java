867d732f40e1d98a136547a897c0c35d6f298ba9
meshramamrapali <amrapalimeshram86@gmail.com>
2023-02-27 14:15:48 +0530

    ADDED : Harmonic Number

--- src/Main.java
+++ src/Main.java
@@ -35,9 +35,18 @@ public class Main {
                 System.out.println("Specified year is not a leap year");
                 System.out.println("-------------------------------------------------");
 
+        double n=10,i;
+        double sum=0;
+        for( i=1;i<=n;i++)
+        {
+            sum=sum+(1/i);
+        }
+        System.out.println("Harmonic value = "+sum);
+        System.out.println("-------------------------------------");
+    }
 
         }
-    }
+
 
 
 
